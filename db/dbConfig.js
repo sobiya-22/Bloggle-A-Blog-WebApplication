33601dc089020c6eb72105089e260cae906355e8
sobiya-22 <sobiyashaikh22@gmail.com>
2024-12-26 12:48:04 +0530

    db chaned

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,10 +1,23 @@
 import mongoose from 'mongoose';
 
 async function connectDB() {
-    mongoose.connect('mongodb://localhost:27017/bloggle').then(() => {
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+        console.error('MongoDB URI is not defined in environment variables.');
+        process.exit(1); // Exit if no URI is provided
+    }
+
+    try {
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('DB Connected!');
-    }).catch((error) => console.error('MongoDb Connection Error:', error));
+    } catch (error) {
+        console.error('MongoDB Connection Error:', error);
+        process.exit(1); // Exit process on connection failure
+    }
 }
 
 export default connectDB;
-
