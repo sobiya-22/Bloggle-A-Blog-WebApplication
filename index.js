f2cb20838a9bf6dbac66da852ed599de12f1b4c7
sobiya-22 <sobiyashaikh22@gmail.com>
2024-12-27 11:26:57 +0530

    done

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import User from './models/userAccess.js';
 import Blog from './models/blogs.js';
 import path from "path";
 import { fileURLToPath } from "url";
-
 const app = express();
 const port = process.env.PORT || 3000; 
 
@@ -17,8 +16,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Set views directory and view engine
-app.set("views", path.join(__dirname, "views"));
+console.log('Current directory:', __dirname);
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.set('views', './views');
 
 app.use(express.static('public'));
@@ -31,14 +33,14 @@ app.get('/', async (req, res) => {
   try {
     if (!req.session.userId) {
       // Redirect to sign-in page if the user is not logged in
-      return res.render('index', { userData: null, allBlogs: [] });
+      return res.render('index.ejs', { userData: null, allBlogs: [] });
     }
 
     const objID = req.session.userId.toString();
     const userData = await User.findById(objID);
     const blogs = await Blog.find({ createdBy: objID });
 
-    res.render('index', {
+    res.render('index.ejs', {
       userData: userData,
       allBlogs: blogs,
     });
